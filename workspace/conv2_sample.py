d5e8a2589a14eec8c77e34899c1a17a3d053aefb
aoinu <aoinu0425@gmail.com>
2022-08-31 17:00:49 +0900

    test

---
+++ workspace/conv2_sample.py
@@ -0,0 +1,26 @@
+import numpy as np
+import wave
+import pandas as pd
+import matplotlib.pyplot as plt
+import os 
+import scipy.io.wavfile as wf
+import FC_fucntion 
+from scipy.signal import kaiserord, lfilter, firwin
+from scipy.fftpack import fft
+
+from scipy.io import wavfile
+from sklearn.svm import SVC
+from scipy.signal import spectrogram
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+
+import glob
+import itertools
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import random
